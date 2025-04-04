3149e8c812ce31b80741fe85ac20273958fa57ec
danielzhang130 <danielzhang130@gmail.com>
2025-04-03 21:44:35 -0400

    added the easy equations. not tested

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,55 @@ import {
 } from "recharts";
 import { toPng } from "html-to-image";
 
+const aircraft = `
+YKF,C152,C-F,1150.69,30.47,35062.11,39,N/A,64,84,42
+YAP,C152LR,C-G,1171.79,29.84,34966.11,39,N/A,64,84,39
+GBN,C152LR,C-G,1178.69,30.10,35477.85,39,N/A,64,84,39
+YHN,C152,C-G,1146.39,30.23,34652.07,39,N/A,64,84,42
+TGH,C152,C-G,1142.29,29.67,33886.90,39,N/A,64,84,42
+ZKT,C152,C-G,1151.64,29.93,34466.32,39,N/A,64,84,42
+GJK,C152,C-F,1170.87,30.29,35463.23,39,N/A,64,84,42
+GFR,A152,C-G,1183.37,31.12,36828.41,39,N/A,64,84,42
+GFV,C172N,C-G,1447.05,39.04,56499.27,37,73,95,123,47.9
+PEL,C172S,C-F,1668.80,39.75,66336.86,37,73,95,123,48
+YEE,C172S,C-G,1708.60,41.17,70341.81,37,73,95,123,48
+LXP,C172S,C-G,1704.49,41.48,70708.48,37,73,95,123,48
+CBN,C172S,C-G,1676.20,39.65,66466.06,37,73,95,123,48
+UBI,C172S,C-G,1674.70,40.16,67263.86,37,73,95,123,48
+RFK,C172S,C-G,1686.60,39.77,67078.58,37,73,95,123,48
+UZZ,C172S,C-G,1678.40,39.88,66935.47,37,73,95,123,48
+PZV,C172S,C-G,1650.00,40.19,66309.92,37,73,95,123,48
+OIQ,C172S,C-G,1652.45,40.09,66252.58,37,73,95,123,48
+AKH,C172S,C-F,1665.00,38.67,64390.80,37,73,95,123,48
+AAR,C172S,C-G,1668.20,40.07,66842.71,37,73,95,123,48
+RAR,C172S,C-F,1704.00,40.97,69808.51,37,73,95,123,48
+ATP,C172S,C-F,1718.27,41.41,71154.12,37,73,95,123,48
+JMD,C172S,C-G,1736.84,41.60,72258.01,37,73,95,123,48
+RZZ,C172S,C-F,1711.67,41.08,70316.68,37,73,95,123,48
+AMO,DA40SR,C-F,1749.35,97.43,170433.53,90.6,128,143.7,170.1,103.5
+IXL,DA40AP,C-G,1800.50,97.80,176097.00,90.6,128,143.7,170.1,103.5
+JUM,DA40,C-F,1788.00,97.51,174347.88,90.6,128,143.7,170.1,103.5
+FTU,P28A,C-F,1822.99,87.04,158669.68,80.5,118.1,142.8,N/A,95
+IZI,P28A,C-G,1816.82,86.78,157655.58,80.5,118.1,142.8,N/A,95
+GPY,P28A,C-G,1828.80,86.42,158047.70,80.5,118.1,142.8,N/A,95
+OLP,PA44,C-G,2653.00,86.13,228515.10,80.5,118.1,142.8,N/A,95
+MOP,PA44,C-G,2670.36,85.69,228822.74,80.5,118.1,142.8,N/A,95
+KUL,PA44,C-F,2674.02,85.59,228857.19,80.5,118.1,142.8,N/A,95
+`
+
+const model = `
+C152,6,0.8,26,24.5,1674.8,1670,1670,1670,31,1100,31,1350,31,1350,31,1350,32.65,1670,32.65,1670,36.5,1670,31,1100,31,1350,31,1350,31,1350,32.65,1670,32.65,1670,36.5,1670
+C152LR,6,0.8,39,37.5,1674.8,1670,1670,1670,31,1100,31,1350,31,1350,31,1350,32.65,1670,32.65,1670,36.5,1670,31,1100,31,1350,31,1350,31,1350,32.65,1670,32.65,1670,36.5,1670
+A152,6,0.8,26,24.5,1674.8,1670,1670,1670,31,1100,31,1350,31,1350,31,1350,32.65,1670,32.65,1670,36.5,1670,31,1100,31,1350,31,1350,31,1350,32.65,1670,32.65,1670,36.5,1670
+C172N,10,1.4,54,50,2308,2300,2300,2000,35,1500,35,1950,35.5,2000,38.5,2300,40.5,2300,40.5,2300,47.3,2300,35,1500,35,1950,35.5,2000,38.5,2000,40.5,2000,40.5,0,47.3,0
+C172S,10,1.4,56,53,2558,2550,2550,2200,35,1500,35,1950,37.5,2200,40.5,2500,40.5,2500,41,2550,47.3,2550,35,1500,35,1950,37.5,2200,40.5,2200,40.5,0,41,0,47.3,0
+DA40SR,10,1.5,41.2,40.2,2655,2646,2407,2161,94.5,1500,94.5,2161,97.6,2646,97.6,2646,97.6,2646,97.6,2646,102,2646,94.5,1500,94.5,2161,94.5,2161,94.5,2161,94.5,2161,94.5,2161,102,2161
+DA40,10,1.5,51,50,2655,2646,2535,2161,94.5,1500,94.5,2161,97.6,2646,97.6,2646,97.6,2646,97.6,2646,100.4,2646,94.5,1500,94.5,2161,94.5,2161,94.5,2161,94.5,2161,94.5,2161,100.4,2161
+DA40AP,10,1.5,51,50,2544,2535,2407,2161,94.5,1500,94.5,2161,96.9,2535,96.9,2535,96.9,2535,96.9,2535,100.4,2535,94.5,1500,94.5,2161,94.5,2161,94.5,2161,94.5,2161,94.5,2161,100.4,2161
+P28A,12,1.4,77,72,2758,2750,2750,0,82,1500,82,2375,88.9,2750,88.9,2750,88.9,2750,88.9,2750,91.5,2750,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+PA44,22,2.7,110,108,3816,3800,3800,0,84,2000,84,2800,85,3400,85,3400,85,3400,89,3800,93,3800,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+`
+
 const SquareDot = (props: any) => {
   const { cx, cy, stroke, strokeWidth } = props;
   return (
@@ -72,8 +121,76 @@ export default function Home() {
   const [data, setData] = useState<GraphData[]>([]);
   const [showGraph, setShowGraph] = useState<boolean>(false);
 
+  // parse aircraft
+
+
   // ============== EQUATIONS SECTION ==============
   const calculateGraphData = (formData: FormData): GraphData => ({
+    const model = lookupModel(formData.aircraftReg);
+	const fuelLoaded = Number(formData.fuelLoaded);
+	const density = 6;
+	const fuelWeight = fuelLoaded * density;
+	const startTaxiFuel = Number(formData.startups) * model.groundFuelRate;
+	const tripFuel = Number(formdata.flightDuration) * model.fuelRate;
+	const landingFuel = fuelLoaded - startTaxiFuel - tripFuel;
+	const reserveTime = 1;
+	const reserveFuel = reserveTime * model.fuelRate;
+	const minDepFuel = reserveFuel + startTaxiFuel + tripFuel;
+	// const maxDepFuel = ;
+	const endurance = (fuelLoaded - startTaxiFuel) / model.fuelRate;
+	
+	// const va = ;
+	// const vref = ;
+	
+	// const utilityMaxFuel = ;
+	// const timeToUtility = ;
+	
+	// WB
+	const weight = [
+		model.weight,
+		Number(formData.frontLeft) + Number(formData.frontRight),
+		Number(formData.rearLeft) + Number(formData.rearRight),
+		Number(formData.bag1),
+		Number(formData.bag2),
+		(fuelLoaded - startTaxiFuel) * density,
+	];
+	const arm = [
+		model.arm,
+		model.frontSeatArm,
+		model.rearSeatArm,
+		model.bag1Arm,
+		model.bag2Arm,
+		model.fuelArm
+	];
+	let takeoffWeight = 0;
+	for (var w of weight) {
+		takeoffWeight += w;
+	}
+	
+	let takeoffMoment = 0;
+	for (let i = 0; i < weight.length; i++) {
+		takeoffMoment += weight[i] * arm[i];
+	}
+	
+	const takeoffArm = takeoffMoment / takeoffWeight;
+	const landingWeight = takeoffWeight - tripFuel * density;
+	const landingMoment = takeoffMoment - tripFuel * density * model.fuelArm;
+	const landingArm = landingMoment / landingWeight;
+	
+	// flags
+	// const overMTOW
+	// const overMLW
+	const belowMinDepFuel = fuelLoaded < minDepFuel;
+	const bag1Over = formData.bag1 > model.bag1Max;
+	const bag2Over = formData.bag2 > model.bag2Max;
+	const bagsOver = formData.bag1 + formData.bag2 > model.bagsMax;
+	const maxFuelInsufficient = maxDepFuel < minDepFuel;
+	
+	const normalCat = /*BJ20*/ > takeoffWeight && fuelLoaded > utilityMaxFuel && fuelLoaded <= maxDepFuel;
+	const utilityCat = takeoffWeight <= model.utilityWeight && fuelLoaded <= utilityMaxFuel;
+	const hasPassenger = weight[2] > 0 || weight[3] > 0 || weight[4] > 0;
+	const noSpin = model.noSpin || utilityMaxFuel < reserveFuel || hasPassenger;
+	
     cg: Number(formData.frontLeft) || 15,
     landingWt: Number(formData.flightDuration)
       ? 1000 + Number(formData.flightDuration) * 10
