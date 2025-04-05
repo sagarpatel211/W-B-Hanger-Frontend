1e9eadee150f763263e50cc22e76220fe415fd4d
Sagar Patel <2sagarpatel2@gmail.com>
2025-04-04 23:14:53 -0400

    Fixing frontend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-dm-sans)", "sans-serif"],
+        sans: ['var(--font-dm-sans)', 'sans-serif'],
       },
     },
   },
